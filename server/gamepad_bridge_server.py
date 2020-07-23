3bda9053242cec454610853a5064a169d525c77b
AG6GR <slhe@alumni.princeton.edu>
2020-07-22 22:45:35 -0700

    Add serial output to server

--- server/gamepad_bridge_server.py
+++ server/gamepad_bridge_server.py
@@ -1,5 +1,6 @@
 import asyncio
 import websockets
+import serial
 from pathlib import Path
 
 class WSServer:
@@ -19,18 +20,18 @@ class WSServer:
     def start(self, num_controllers, base_port):
         for i in range(num_controllers):
             print("Starting server on port", i + args.base_port)
-            asyncio.get_event_loop().run_until_complete(websockets.serve(self.on_receive, "localhost", i + base_port))
+            asyncio.get_event_loop().run_until_complete(websockets.serve(self.on_receive, "", i + base_port))
             print("Server started")
 
 # Consumes arriving messages queue. This serializes message handling independent
 # of the server threads, to avoid races over the serial output
-async def queue_handler(queue, outfilepath):
+async def queue_handler(queue, serial_port, baud=115200):
     print("queue_handler started")
-    with open(outfilepath, 'wb') as outfile:
+    with serial.Serial(serial_port, 115200, timeout=1) as ser:
         while True:
             index, message = await queue.get()
             print("Dequeued message", message, "for index", index)
-            outfile.write(message)
+            ser.write(message)
             queue.task_done()
 
 if __name__ == '__main__':
@@ -39,7 +40,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run server for Remote USB Gamepad bridge')
     parser.add_argument('--num-controllers', type=int, default=2, help='Number of controllers to listen for')
     parser.add_argument('--base-port', '-p', type=int, default=8000, help='Base port number for 0th controller. Other controllers will have servers attached to port base+id')
-    parser.add_argument('outfile', type=Path, help="Output file to log incomming messages")
+    parser.add_argument('--baud', type=int, default=115200, help='Serial port baud rate')
+    parser.add_argument('serial_port', help="Serial port to output commands")
 
     args = parser.parse_args()
 
@@ -48,5 +50,5 @@ if __name__ == '__main__':
     server = WSServer(queue)
     server.start(args.num_controllers, args.base_port)
 
-    asyncio.get_event_loop().run_until_complete(queue_handler(queue, args.outfile))
+    asyncio.get_event_loop().run_until_complete(queue_handler(queue, args.serial_port, args.baud))
     asyncio.get_event_loop().run_forever()
