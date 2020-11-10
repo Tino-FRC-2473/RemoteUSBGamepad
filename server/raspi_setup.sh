cf54ec661351cbca4deb9341aab4bc7b1c86e0d1
AG6GR <slhe@alumni.princeton.edu>
2020-11-10 15:07:04 -0800

    Add systemd service config
    Install systemd service called remoteusbserver.service to allow
    automatic launching of the server on reboot and unified logging.

---
+++ server/raspi_setup.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Hardware setup
+sudo raspi-config nonint do_expand_rootfs
+sudo raspi-config nonint do_serial 1
+
+# General update
+sudo apt update
+sudo apt upgrade -y
+sudo reboot
+
+# Packages
+sudo apt install -y git vim pipenv tmux
+
+# Setup RemoteUSBGamepad
+git clone https://github.com/Tino-FRC-2473/RemoteUSBGamepad.git
+pushd RemoteUSBGamepad
+pipenv install
+
+# Install systemd service
+sudo cp server/remoteusbserver.service /etc/systemd/system/remoteusbserver.service
+sudo systemctl enable remoteusbserver.service
+sudo systemctl start remoteusbserver.service
+
+# Check status with systemctl status remoteusbserver.service
+# View logs with journalctl -f -u remoteusbserver.service
